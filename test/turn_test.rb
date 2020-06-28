e6fdef7c5f398dc9d403925542f2540d15c20806
Saryn Mooney <smooney15@my.whitworth.edu>
2020-06-28 10:14:54 -0600

    turn class created

---
+++ test/turn_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
+require './lib/card'
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_instance_of Turn, turn
+  end
+
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_it_returns_the_correct_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal card, turn.card
+  end
+
+  def test_it_knows_if_guess_is_correct_answer
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal true, turn.correct?
+    turn = Turn.new("Anchorage", card)
+    assert_equal false, turn.correct?
+  end
+
+  def test_it_returns_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Correct!", turn.feedback
+    turn = Turn.new("Anchorage", card)
+    assert_equal "Incorrect", turn.feedback 
+  end
+
+
+
+end
